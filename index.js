ef0905d8ca1c71da872884c3de5a3523fac8cda0
Zachary Margulies <zmarg1@umbc.edu>
2024-06-18 10:27:45 -0400

    updatated formatting of buttons to grid form

--- index.js
+++ index.js
@@ -11,30 +11,45 @@ class App extends React.Component{
     render(){
         if (this.state.page === 'Home') {
             return (
-                <div className="text-center">
-                    <p>HOME PAGE RENDERED</p>
-                    <button onClick={() => {this.setState({page:'Patients'})}} className="btn btn-primary btn-lg m-1">Patient Info</button>
-                    <button onClick={() => {this.setState({page:'Alarms'})}} className="btn btn-primary btn-lg m-1">Alarms</button>
-                    <PowerButton 
-                        power={this.state.power}
-                        onPowerChange={this.handlePowerChange}
-                    />
+                <div className="container">
+                    <h1 className="bg-info text-white text-center">HOME PAGE RENDERED</h1>
+                    <div className="row">
+                        <div className="col-sm-4 text-center">
+                            <PowerButton 
+                                power={this.state.power}
+                                onPowerChange={this.handlePowerChange}
+                        />
+                        </div>
+                        <div className="col-sm-8 text-start">
+                            <button onClick={() => {this.setState({page:'Patients'})}} className="btn btn-primary btn-lg m-1">Patient Info</button>
+                            <button onClick={() => {this.setState({page:'Alarms'})}} className="btn btn-primary btn-lg m-1">Alarms</button>
+                        </div>
+                        
+                    </div>
                 </div>
             );
         }
         else if (this.state.page === 'Patients') {
             return (
-                <div className="text-center">
-                    <p>PATIENT INFO PAGE RENDERED</p>
-                    <button onClick={() => {this.setState({page:'Home'})}} className="btn btn-primary btn-lg m-1">Back</button>
-                </div>
+                <div className="container">
+                        <h1 className="bg-info text-white text-center">PATIENT INFO PAGE RENDERED</h1>
+                        <div className="row text-center">
+                            <div className="col-sm-12">
+                            <button onClick={() => {this.setState({page:'Home'})}} className="btn btn-primary btn-lg m-1">Back</button>
+                            </div>
+                        </div>
+                    </div>
             );
         }
         else if (this.state.page === 'Alarms'){
             return (
-                <div className="text-center">
-                    <p>ALARM PAGE RENDERED</p>
-                    <button onClick={() => {this.setState({page:'Home'})}} className="btn btn-primary btn-lg m-1">Back</button>
+                <div className="container">
+                    <h1 className="bg-info text-white text-center">ALARMS PAGE RENDERED</h1>
+                    <div className="row text-center">
+                        <div className="col-sm-12">
+                            <button onClick={() => {this.setState({page:'Home'})}} className="btn btn-primary btn-lg m-1">Back</button>
+                        </div>
+                    </div>
                 </div>
             );
         }
